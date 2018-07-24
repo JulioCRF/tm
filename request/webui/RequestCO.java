19c3ad45ba63ba4acf6ccd7151657201ff00e30a
Julio <julioc.esfm@gmail.com>
2018-07-24 17:11:12 -0500

    test github

--- request/webui/RequestCO.java
+++ request/webui/RequestCO.java
@@ -49,6 +49,7 @@ public class RequestCO extends OAControllerImpl
     else{
         System.out.println("Debug 2");
         System.out.println("Hola git");
+        System.out.println("hola github");
     }//fin de else
   }//fin de processRequest
 
